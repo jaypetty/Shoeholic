b304920cb9964021e10701bf0217cc955685b6eb
jaypetty <jayshaun2343@gmail.com>
2022-05-03 10:07:40 -0500

    added shoe model

--- Shoeholic/Models/Shoe.cs
+++ Shoeholic/Models/Shoe.cs
@@ -1,6 +1,24 @@
-﻿namespace Shoeholic.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shoeholic.Models
 {
     public class Shoe
     {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int BrandId { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        public int RetailPrice { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Colorway { get; set; }
+        public int CollectionId { get; set; }
+        public Brand Brand { get; set; }
     }
 }
